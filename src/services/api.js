dbdaecd4bfb09f58eb5cf7f55b74b03d894c7db4
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-06-25 14:06:08 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,22 +5,44 @@ const API = axios.create({
     import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
+
+// REQUEST INTERCEPTOR
 API.interceptors.request.use(
   (config) => {
     const token =
       sessionStorage.getItem("adminToken") ||
       sessionStorage.getItem("token");
 
-    
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-      
     }
 
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+
+// RESPONSE INTERCEPTOR
+API.interceptors.response.use(
+
+  (response) => response,
+
+  (error) => {
+
+    // normal user deleted OR invalid token
+    if (
+      error.response?.status === 401 ||
+      error.response?.status === 404
+    ) {
+      sessionStorage.removeItem("token");
+      sessionStorage.removeItem("user");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default API;
