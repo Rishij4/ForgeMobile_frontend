341532f8b20d556ae478a9e651b8cb50d107658f
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-06-26 15:03:37 +0530

    Update BuildSummaryCard.jsx

--- src/components/lab/BuildSummaryCard.jsx
+++ src/components/lab/BuildSummaryCard.jsx
@@ -67,14 +67,14 @@ const BuildSummaryCard = ({
       </motion.div>
 
       <motion.div variants={itemVariants} className="mt-8 bg-green-900/20 border border-green-500 rounded-2xl p-6 shadow-inner">
-        <h3 className="text-xl sm:text-2xl font-bold text-green-400 tracking-tight">Total Build Price: ₹{totalPrice?.toLocaleString()}</h3>
+        <h3 className="text-xl sm:text-2xl font-bold text-green-400 tracking-tight">Estimated Price: ₹{totalPrice?.toLocaleString()}</h3>
       </motion.div>
       <motion.div
         variants={itemVariants}
         className="mt-4 bg-blue-900/20 border border-blue-500 rounded-2xl p-6 shadow-inner"
       >
         <h3 className="text-xl sm:text-2xl font-bold text-blue-400 tracking-tight">
-          Market Price: ₹{marketPrice ? marketPrice.toLocaleString() : "Not Available"}
+          Market Price: ₹{marketPrice ? marketPrice.toLocaleString() : "Run Test For Market Price"}
         </h3>
       </motion.div>
     </motion.div>
